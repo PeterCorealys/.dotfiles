3c4982db36d15ea19279d09c4acec9f115e402e9
Peter <peter@localhost.localdomain>
2021-02-21 19:53:29 +0100

    Fedora Install Script

---
+++ Fedora-Install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function install {
+  which $1 &> /dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${1}..."
+    dnf install $1
+  else
+    echo "Already installed: ${1}"
+  fi
+}
+
+install awesome
+install rofi
+install compton
+
+
+install vim
+install nvim
+install git
+
+install terminator 
+install tmux
+
+install neofetch
+install lolcat
+install htop
+install tree
